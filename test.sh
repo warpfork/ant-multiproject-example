3da0ec1c80ed9d9b02afe4deeb49e125fbbf3a01
Eric Myhre <hash@exultant.us>
2014-07-13 22:23:25 -0500

    make a distinction between subprojects with real steps and nodes that just gather subprojects.  this will make it possible for the root project to pass, despite not itself having the same meaningful targets as a subproject.
    (admittedly, the fact that 'dist' and 'compile' are different numbers here is sort of another artifact of its own, but it's good enough to demonstrate basic operation.)

--- test.sh
+++ test.sh
@@ -2,15 +2,16 @@
 
 function testProject {(
 	PROJECT=$1
-	DEPCOUNT=$2
+	NPROJECT=$2
+	NNODE=$3
 
 	cd $PROJECT
-	output=`ant`
+	output=`ant dist`
 
 	echo ====
-	echo -n "project '$PROJECT' occurances of 'compile'    ...   " ; echo "$output" | checkMatchCount "^compile:$" "$DEPCOUNT"
-	echo -n "project '$PROJECT' occurances of 'dist'       ...   " ; echo "$output" | checkMatchCount "^dist:$"    "$DEPCOUNT"
-	echo -n "project '$PROJECT' occurances of 'delegate.*' ...   " ; echo "$output" | checkMatchCount "^delegate." "$(($DEPCOUNT * 2 - 1))"
+	echo -n "project '$PROJECT' occurances of 'compile'    ...   " ; echo "$output" | checkMatchCount "^compile:$" "$NPROJECT"
+	echo -n "project '$PROJECT' occurances of 'dist'       ...   " ; echo "$output" | checkMatchCount "^dist:$"    "$NNODE"
+	echo -n "project '$PROJECT' occurances of 'delegate.*' ...   " ; echo "$output" | checkMatchCount "^delegate." "$(($NNODE * 2 - 1))"
 	echo -n "project '$PROJECT' has all unique delegates   ...   " ; [ 0 -eq `echo "$output" | grep "^delegate." | sort | uniq -d | wc -l` ] && echo "PASS" || echo "FAIL"
 	echo -n "project '$PROJECT' has all unique delegatedeps...   " ; [ 0 -eq `echo "$output" | grep "^delegate.*.dependencies:$" | sort | uniq -d | wc -l` ] && echo "PASS" || echo "FAIL"
 	echo ====
@@ -27,10 +28,10 @@ function checkMatchCount {
 	[ $NEXPECT -eq $nmatch ] && echo "PASS, $nmatch/$NEXPECT" || { echo "FAIL, $nmatch/$NEXPECT" ; echo "  actual matching outputs:" ; echo "$matches" | sed 's/^/    /' ; }
 }
 
-testProject web 3
-testProject admin 3
-testProject model 2
-testProject utilities 1
-testProject . 4
+testProject web       3 3
+testProject admin     3 3
+testProject model     2 2
+testProject utilities 1 1
+testProject .         4 5
 
 
