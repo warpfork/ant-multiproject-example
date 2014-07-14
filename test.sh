ca19e1bca9275c5d4a9598c302ca8ffc958c1557
Eric Myhre <hash@exultant.us>
2014-07-13 21:29:23 -0500

    write a bash script to test the ant builds are sufficiently but not excessively recursive.  so meta.

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function testProject {(
+	PROJECT=$1
+	DEPCOUNT=$2
+
+	cd $PROJECT
+	output=`ant`
+
+	echo ====
+	echo -n "project '$PROJECT' occurances of 'compile'    ...   "
+	[ $DEPCOUNT -eq `echo "$output" | grep "^compile:$" -c` ] && echo "PASS" || echo "FAIL"
+	echo -n "project '$PROJECT' occurances of 'dist'       ...   "
+	[ $DEPCOUNT -eq `echo "$output" | grep "^dist:$" -c` ] && echo "PASS" || echo "FAIL"
+	echo -n "project '$PROJECT' occurances of 'delegate.*' ...   "
+	[ $DEPCOUNT -eq `echo "$output" | grep "^delegate." -c` ] && echo "PASS" || echo "FAIL"
+	echo ====
+)}
+
+testProject web 3
+testProject admin 3
+testProject model 2
+testProject utilities 3
+testProject . 4
+
+
