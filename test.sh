d60cadda889fb70a8b0801f1cfa617cead7d70c3
Eric Myhre <hash@exultant.us>
2014-07-13 22:04:38 -0500

    upgrade test script to both be awesomer and also list the captured output if it didn't meet expectations.

--- test.sh
+++ test.sh
@@ -8,15 +8,21 @@ function testProject {(
 	output=`ant`
 
 	echo ====
-	echo -n "project '$PROJECT' occurances of 'compile'    ...   "
-	[ $DEPCOUNT -eq `echo "$output" | grep "^compile:$" -c` ] && echo "PASS" || echo "FAIL"
-	echo -n "project '$PROJECT' occurances of 'dist'       ...   "
-	[ $DEPCOUNT -eq `echo "$output" | grep "^dist:$" -c` ] && echo "PASS" || echo "FAIL"
-	echo -n "project '$PROJECT' occurances of 'delegate.*' ...   "
-	[ $DEPCOUNT -eq `echo "$output" | grep "^delegate." -c` ] && echo "PASS" || echo "FAIL"
+	echo -n "project '$PROJECT' occurances of 'compile'    ...   " ; echo "$output" | checkMatchCount "^compile:$" "$DEPCOUNT"
+	echo -n "project '$PROJECT' occurances of 'dist'       ...   " ; echo "$output" | checkMatchCount "^dist:$"    "$DEPCOUNT"
+	echo -n "project '$PROJECT' occurances of 'delegate.*' ...   " ; echo "$output" | checkMatchCount "^delegate." "$DEPCOUNT"
 	echo ====
 	echo
 )}
+
+function checkMatchCount {
+	# stdin provides stream to match over
+	PATTERN=$1
+	NEXPECT=$2
+
+	matches=`grep "$PATTERN"`
+	[ $NEXPECT -eq `echo "$matches" | wc -l` ] && echo "PASS" || { echo "FAIL" ; echo "  actual matching outputs:" ; echo "$matches" | sed 's/^/    /' ; }
+}
 
 testProject web 3
 testProject admin 3
